b94074de3072dcd985288c8076778187efd26aa0
EM-engineer <66124569+EM-engineer@users.noreply.github.com>
2023-05-04 15:02:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
-from setuptools import setup
+#from setuptools import setup
 
-if __name__ == "__main__":
-    setup()
+#if __name__ == "__main__":
+#    setup()
+from setuptools import setup, find_packages
+
+setup(
+    name="dalle-mini",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "jax>=0.4.8",  # Modify the JAX version range according to your needs
+        "transformers",
+        "vqgan-jax",
+        "wandb",
+    ],
+)
